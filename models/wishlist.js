ed7cee934f702fa15a6342b1304181a7d8ce958f
Abhinash <avinash.sharmaz16.1995@gmail.com>
2019-12-20 12:14:04 +0530

    'abhinash'

--- models/wishlist.js
+++ models/wishlist.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Joi = require('joi');
 
 const wishlistSchema = new mongoose.Schema({
-    v_id:String,
+    v_id:Number,
     c_id: String,
 });
 const Wishlist = mongoose.model('wishlist', wishlistSchema)
 
 function validatewishlist(wishlist) {
     const schema = {
-      v_id: Joi.string().min(1).max(100).required(),
+      v_id: Joi.number().min(1).max(100).required(),
       c_id: Joi.string().min(1).max(500).required()
     };
   
